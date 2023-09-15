92ff5f83ede268f37f6ba18a457c6a1cf7a344af
Tomibusch1 <tomibusch5@gmail.com>
2023-09-15 19:37:05 -0300

    added feature to main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Helloo World");
-        system.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+
+        File file = new File(
+            "..\\config\\secret.txt");  
+
+        try (
+        BufferedReader br = new BufferedReader(new FileReader(file))) {
+            
+            String st;
+            
+            while ((st = br.readLine()) != null)
+
+                System.out.println(st);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
