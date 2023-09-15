32147ac0d4df528e08727ebf0b3b5d5dd7267b58
Tomibusch1 <tomibusch5@gmail.com>
2023-09-15 20:00:48 -0300

    change B

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("Hola mundo");
 
         System.out.println("Hola mundo");
+        System.out.println("Hello World");
 
         File file = new File(
             "..\\config\\secret.txt");  
